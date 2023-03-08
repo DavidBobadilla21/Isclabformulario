b6af0af6a5d2be58a43e03c9d0f7938d70df62a3
unknown <david.19070008@itsmotul.edu.mx>
2023-03-07 23:34:34 -0600

    version.1.1

--- API/src/App.vue
+++ API/src/App.vue
@@ -4,82 +4,40 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header class=" bg-blue-700">
+      <div class="wrapper">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <nav class=" bg-blue-700">
+          <div class="grid grid-cols-3">
+            <div></div>
+            <div class=" bg-blue-700">
+              <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+            </div>
+            <div></div>
+          </div>
+        </nav>
+      </div>
+      <div class="grid grid-cols-5 items-center text-xxl text-white">
+        <div></div>
+        <div class=""> <button
+            class=" bh-14 sm:h-10 px-7 font-semibold rounded-md bg-green-800 mb-8 justify-center shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+            <RouterLink to="/">Home</RouterLink>
+          </button></div>
+        <div class="..."><button
+            class="h-14 sm:h-10 px-7 font-semibold rounded-md bg-green-800  mb-8 justify-center shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+            <RouterLink to="/about">About</RouterLink>
+          </button></div>
+        <div class="..."><button
+            class="h-14 sm:h-10 px-7 font-semibold rounded-md bg-green-800  mb-8 justify-center shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+            <RouterLink to="/about" class="py-5">Ver ingresados</RouterLink>
+          </button></div>
+        <div></div>
+      </div>
+    </header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+
+
+
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ API/src/components/Formulario.vue
@@ -0,0 +1,84 @@
+<script>
+import axios from "axios";
+let API_URL = `/api/guardarPersonasUxd.php`;
+export default {
+  data() {
+    return {
+      nombre: "",
+      edad: "",
+      estadoCivil: "",
+      trabajo: "",
+      residencia: "",
+      cita: "",
+      citaAutor: "",
+      bio: "",
+      personalidad01: "",
+      personalidad02: "",
+      personalidad03: "",
+      personalidad04: "",
+      objetivos: "",
+      frustraciones: "",
+      motivaciones: "",
+      porcentaje: "",
+      marcas: "",
+    };
+  },
+  methods: {
+    summit() {
+      API_URL + this.pagina;
+      console.log(API_URL);
+      axios.post("/api/guardarPersonasUxd.php", {
+        nombre: this.nombre,
+        edad: this.edad,
+        estadoCivil: this.estadoCivil,
+        trabajo: this.trabajo,
+        residencia: this.residencia,
+        cita: this.cita,
+        citaAutor: this.citaAutor,
+        bio: this.bio,
+        personalidad01: this.personalidad01,
+        personalidad02: this.personalidad02,
+        personalidad03: this.personalidad03,
+        personalidad04: this.personalidad04,
+        objetivos: this.objetivos,
+        frustraciones: this.frustraciones,
+        motivaciones: this.motivaciones,
+        porcentaje: this.porcentaje,
+        marcas: this.marcas,
+        //completar las variables, estas deben llamarse como las que se recibirán en el backend sin el símbolo del dolar $
+      })
+        .then((response) => {
+          console.log(response.status)
+        });
+    },
+  },
+};
+
+</script>
+<template>
+  <div class="grid grid-cols-3">
+    <div></div>
+    <div>
+      <form class="w-full max-w-lg">
+
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="nombre">
+              NOMBRE
+            </label>
+            <input v-model="nombre"
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-grey-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="nombre" type="text" placeholder="Jane">
+          </div>
+        </div>
+
+        <button @click="summit()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          ENVIAR
+
+        </button>
+
+      </form>
+    </div>
+    <div> </div>
+  </div>
+</template>
